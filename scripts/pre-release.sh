553969a5a5916cdf9e13e173134653266847b1fa
Diego Osse Fernandes <diego.osse@gmail.com>
2016-05-11 16:45:08 -0300

    Set Git ID on prerelease

--- scripts/pre-release.sh
+++ scripts/pre-release.sh
@@ -3,10 +3,12 @@
 
 if ([ $TRAVIS_BRANCH == $DEV_BRANCH ] && [ $TRAVIS_PULL_REQUEST == "false" ]); then
 
+
+      git config --global user.email "travis@luizalabs.com"
+      git config --global user.name "Travis CI"
+
       # Release a prerelease version
       npm version prerelease --git-tag-version -m "Pre Release %s [ci skip]" -f
-
-      git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 
       # Get updated build version
       BUILD_VERSION=$(node -pe 'JSON.parse(process.argv[1]).version' "$(cat $TRAVIS_BUILD_DIR/package.json)")
