c5961679a60c4bf7e61798657221a5a6c0779edb
Diego Osse Fernandes <diego.osse@gmail.com>
2016-05-20 15:23:24 -0300

    script fix

--- scripts/sonar.sh
+++ scripts/sonar.sh
@@ -1,15 +1,9 @@
 #! /bin/bash
 
-set -euo pipefail
-
-function installSonnarScanner {
-  mkdir ~/sonar-scanner
-  curl -sSL https://sonarsource.bintray.com/Distribution/sonar-scanner-cli/sonar-scanner-2.6.1.zip | tar -xf- --strip-components 1 -C ~/sonar-scanner
-
-}
-
-
-function sonar {
+if ([[ $TRAVIS_PULL_REQUEST = "true" ]]);
+then
+  mkdir ~/sonar-scanner &&
+  curl -sSL https://sonarsource.bintray.com/Distribution/sonar-scanner-cli/sonar-scanner-2.6.1.zip | tar -xf- --strip-components 1 -C ~/sonar-scanner &&
   ~/sonar-scanner/bin/sonar-scanner \
           -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
           -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
@@ -19,11 +13,4 @@ function sonar {
           -Dsonar.projectKey=$TRAVIS_REPO_SLUG \
           -Dsonar.projectName=$(node -pe 'JSON.parse(process.argv[1]).name' "$(cat $TRAVIS_BUILD_DIR/package.json)") \
           -Dsonar.projectVersion=$(node -pe 'JSON.parse(process.argv[1]).version' "$(cat $TRAVIS_BUILD_DIR/package.json)")
-}
-
-
-if ([[ $TRAVIS_PULL_REQUEST = "true" ]]);
-then
-  installSonnarScanner
-  sonar
 fi
